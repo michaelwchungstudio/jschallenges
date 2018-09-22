824bc6c6082c069e3eb5c7c161ee4eac721f4f10
Michael W Chung <dryhslaw@gmail.com>
2018-09-22 18:16:56 -0400

    challenge1 html and css done

---
+++ challenge1/index1.js
@@ -0,0 +1,12 @@
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+
+var showtext = document.getElementById('showtext');
+
+function changeText() {
+
+  }
+
+button1.addEventListener('click', changeText() {
+
+})
